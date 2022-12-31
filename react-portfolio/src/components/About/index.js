0c33bbb03cc85d8e459b38d21567c6ef2c825fe6
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2022-12-30 22:27:36 -0600

    made a cube animation using icons of my top skills

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,6 +1,10 @@
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
 import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faCcAmex, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+
 
 const About = () => {
     const [letterClass,setLetterClass] = useState('text-animate')
@@ -32,6 +36,30 @@ const About = () => {
                 <p>
                     I am a family person and a soccer fanatic. VAMOS MONTERREY!!!
                 </p>
+            </div>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>   
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faCcAmex} color='Blue'/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color='#28A4D9'/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faReact} color='#5ED4F4'/>
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D'/>
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#EC4D28'/>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     )
