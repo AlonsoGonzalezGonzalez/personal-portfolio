8b14bb4113aeb2afcf4af3e783bab2447e4db495
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2022-12-31 23:30:10 -0600

    added a loader icon (pacman)  from react loaders library

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import Headshot from '../../assets/images/headshot.JPG'
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
 const Home = () => {
     const [letterClass,setLetterClass] = useState('text-animate')
     const nameArray = ['A','l','o','n','s','o']
@@ -15,6 +16,7 @@ const Home = () => {
 
 
     return( 
+        <>
         <div className="container home-page">
             <div className="text-zone">
             <h1>
@@ -42,7 +44,8 @@ const Home = () => {
             <img src={Headshot} alt='headshot' ></img>
 
         </div>
-        
+        <Loader type='pacman'></Loader>
+        </>
     )
 }
 
